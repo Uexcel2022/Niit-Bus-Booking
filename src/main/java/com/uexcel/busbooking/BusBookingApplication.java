21987555cf1876629652ee295021e93a84b1e487
Uexcel2022 <excellenceudoka@gmail.com>
2024-05-15 11:07:41 +0100

    project initialization

---
+++ src/main/java/com/uexcel/busbooking/BusBookingApplication.java
@@ -0,0 +1,13 @@
+package com.uexcel.busbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uexcel/busbooking/BusBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uexcel.busbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
